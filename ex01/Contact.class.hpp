4983552420bb0f5a28b6049cf2903e0994691ca3
Valentin Favard <vafavard@f6r11s13.clusters.42paris.fr>
2025-10-06 15:40:28 +0200

    up

---
+++ ex01/Contact.class.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.class.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 14:50:57 by vafavard          #+#    #+#             */
+/*   Updated: 2025/10/06 15:34:06 by vafavard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_CLASS_H
+# define CONTACT_CLASS_H
+
+class   Contact{
+
+public:
+
+	Contact(void);
+	~Contact(void);
+
+private:
+	
+
+};
+
+#endif
---
+++ ex01/PhoneBook.class.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 14:42:59 by vafavard          #+#    #+#             */
+/*   Updated: 2025/10/06 15:36:32 by vafavard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+#include "Contact.class.hpp"
+
+class PhoneBook {
+
+public:
+
+	PhoneBook(void);
+	~PhoneBook(void);
+private:
+	Contact list[8];
+	int	NbContacts;
+
+};
+
+#endif
