85b593295e1b74e0f3ed83c7bd3985c69e053775
Lefav <vfavardd@gmail.com>
2025-10-09 00:48:40 +0200

    up

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:17:19 by vafavard          #+#    #+#             */
-/*   Updated: 2025/10/07 16:32:07 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/10/09 00:47:04 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,19 @@
 #include <string>
 #include <iostream>
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
 Account::Account(void){};
 
 Account::~Account(void){};
 
-Account::Account(int initial_deposit){};
-
+Account::Account(int initial_deposit)
+{
+        
+}
 int	Account::getNbAccounts(void)
 {
     return (_nbAccounts);
@@ -44,3 +51,24 @@ void	Account::displayAccountsInfos(void)
 {
     std::cout << "Index = " << _nbAccounts << std::endl;
 }
+
+void    Account::displayStatus(void)const
+{
+
+}
+
+void    Account::makeDeposit(int deposit)
+{
+
+}
+
+bool    Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int Account::checkAmount(void)const
+{
+    
+}
+
