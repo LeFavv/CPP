616f475001da55a11d3f48846730ef57359e7a2e
Votre Nom <Vous@exemple.com>
2025-10-12 16:04:05 +0200

    directives pour faire l'exo 04

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 15:06:16 by vafavard          #+#    #+#             */
-/*   Updated: 2025/10/12 15:31:32 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/10/12 16:03:33 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,31 @@
 #include <string>
 #include <cctype>
 
+//my bad ne pas creer le fichier a la base mais seulement open
+//seulement le fichier .replace doit etre creer
+//std::strinh::find
+//std::string::substr
+//std::ifstream pour ouvrir le fichier 
+//std::getline pour recuperer les lignes
+//std::ofstream pour ecrire dans un nouveau fichier
+
 int main(int argc, char **argv)
 {
     std::ofstream outfile (argv[1]);
 
-    std::ofstream test;
-    // std::string test = strcat(outfile.c_str(), ".replace");
+    std::string test1 = argv[1];
+    std::string test2 = ".replace";
+    std::string test3 = test1 + test2;
+    std::ofstream test (test3);
+    
     outfile << "ceci est un test" << std::endl;
-    test << outfile.rdbuf();
-    // std::cout << test ;
     outfile.close();
+    
+    std::ifstream infile (argv[1]);
+    
+    test << infile.rdbuf();
+
+    infile.close();
     test.close();
     
     return 0;
