87e9b557671e6c8d050dbdf744f757db6e8ac822
Lefav <vfavardd@gmail.com>
2025-12-01 11:09:12 +0100

    CPP 07 ex00

---
+++ CPP_07/ex00/whatever.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 11:00:10 by vafavard          #+#    #+#             */
+/*   Updated: 2025/12/01 11:07:19 by vafavard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template<typename T>
+void    swap(T & a, T & b)
+{
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template<typename T>
+T min(T const & a, T const & b)
+{
+    return (a < b ? a : b);
+}
+
+template<typename T>
+T max(T const &a, T const &b)
+{
+    return (a > b ? a : b);
+}
+
+#endif
