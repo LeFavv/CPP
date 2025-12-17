e08b388653d07f0b9c17ca547b6d25fcfd04e7c0
Valentin Favard <vafavard@f6r9s15.clusters.42paris.fr>
2025-12-17 14:53:58 +0100

    CPP 08 done

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,7 +6,75 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 10:50:01 by vafavard          #+#    #+#             */
-/*   Updated: 2025/12/17 10:50:01 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/12/17 14:35:10 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+template< typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+        MutantStack(void) : std::stack<T>() {};
+        MutantStack(MutantStack const &copy) : std::stack<T>(copy){};
+        ~MutantStack(void){};
+        MutantStack &operator=(MutantStack const &other)
+        {
+            std::stack<T>::operator=(other);
+            return (*this);
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+        
+        iterator begin()
+        {
+            return (this->c.begin());
+        }
+        const_iterator cbegin() const
+        {
+            return (this->c.cbegin());
+        }
+        reverse_iterator rbegin()
+        {
+            return (this->c.rbegin());
+        }
+        
+        const_reverse_iterator crbegin() const
+        {
+            return (this->c.crbegin());
+        }
+        
+        iterator end()
+        {
+            return (this->c.end());
+        }
+        const_iterator cend() const
+        {
+            return (this->c.cend());
+        }
+        reverse_iterator rend()
+        {
+            return (this->c.rend());
+        }
+        
+        const_reverse_iterator crend() const
+        {
+            return (this->c.crend());
+        }
+
+        
+private:
+
+};
+
+#endif
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,57 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 10:49:39 by vafavard          #+#    #+#             */
-/*   Updated: 2025/12/17 10:49:39 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/12/17 14:50:01 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+#include <list>
+
+int main()
+{
+    std::cout << "=================TEST 1 - MUTANTSTATCK ====================" << std::endl;
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    
+    std::cout << "=================TEST 2 - STD::LIST ====================" << std::endl;
+    std::list<int> lst1;
+    lst1.push_back(5);
+    lst1.push_back(17);
+    std::cout << lst1.back() << std::endl;
+    lst1.pop_back();
+    std::cout << lst1.size() << std::endl;
+    lst1.push_back(3);
+    lst1.push_back(5);
+    lst1.push_back(737);
+    lst1.push_back(0);
+    std::list<int>::iterator it1 = lst1.begin();
+    std::list<int>::iterator ite1 = lst1.end();
+    ++it1;
+    --it1;
+    while (it1 != ite1)
+    {
+    std::cout << *it1 << std::endl;
+    ++it1;
+    }
+    std::stack<int> s(mstack);
+    return 0;
+}
